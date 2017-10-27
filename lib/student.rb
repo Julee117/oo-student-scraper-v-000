5ed0f184b7c01ad5c9646dd77876ca9eeb7aaa79
Julianne Kim <juliannne@gmail.com>
2017-10-27 18:23:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |key, value|
       # send("#{key}=", value)
-      self[key] = value 
+      self[key] = value
     end
     self
   end
